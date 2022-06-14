76a2a9840d6c5dea923919942c5670dda4854f9e
zakharov-ievgen <zakharov.ievgen.ukr@gmail.com>
2022-06-14 22:53:13 +0300

    2nd edition

--- javascript/main.js
+++ javascript/main.js
@@ -2,19 +2,27 @@ const sp1 = document.querySelector(`.span1`);
 const sp2 = document.querySelector(`.span2`);
 const sp3 = document.querySelector(`.span3`);
 const sp4 = document.querySelector(`.span4`);
-let Input = document.querySelector(`.data`);
-Input.oninput = function CheckValue() {
-    str = Input.value;
-    if (/[A-Z+]/.test(str)) {
+let input = document.querySelector(`.data`);
+input.oninput = function CheckValue() {
+    let str = input.value;
+    if (/[А-ЯЁA-Z]/.test(str)) {
         sp1.classList.add(`match`);
+    } else {
+        sp1.classList.remove(`match`);
     }
     if (/\d+/.test(str)) {
         sp2.classList.add(`match`);
+    } else {
+        sp2.classList.remove(`match`);
     }
     if (/[^\d\sA-Z]/gi.test(str)) {
         sp3.classList.add(`match`);
+    } else {
+        sp3.classList.remove(`match`);
     }
     if (str.length > 8) {
         sp4.classList.add(`match`);
+    } else {
+        sp4.classList.remove(`match`);
     }
 };
